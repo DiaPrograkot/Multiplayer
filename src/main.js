e54c63e702f9da7c50f0b5da2a7f2b75a1dc15ff
DiaPrograkot <popelenko200290@gmail.com>
2024-09-14 23:22:19 +0300

    Попытка отслеживания входа по имени

--- src/main.js
+++ src/main.js
@@ -9,9 +9,26 @@ const config = {
 // Инициализация и присоединение к комнате
 const room = joinRoom(config, 'room-id'); // Замените 'room-id' на ваш реальный roomId
 
+// Извлечение имени игрока из localStorage
+const nameStorage = localStorage.getItem("name");
+
 // Пример использования функции joinRoom
-room.onPeerJoin(peerId => console.log(`${peerId} joined`));
-room.onPeerLeave(peerId => console.log(`${peerId} left`));
+room.onPeerJoin(peerId => {
+  if (nameStorage) {
+    console.log(`${nameStorage} joined`);
+  } else {
+    console.log(`${peerId} joined`);
+  }
+});
+
+room.onPeerLeave(peerId => {
+  if (nameStorage) {
+    console.log(`${nameStorage} left`);
+  } else {
+    console.log(`${peerId} left`);
+  }
+});
+
 room.onPeerStream((stream, peerId) => {
   const video = document.createElement('video');
   video.srcObject = stream;
