48df18efbee6247d501f3d3fe6b8508ec85402ab
lera <lerochkaaa15@gmail.com>
2024-09-26 03:11:20 +0300

    attempt 1

--- src/main.js
+++ src/main.js
@@ -10,9 +10,8 @@ function addMessage(message) {
   const messageBox = document.querySelector('.messages'); // Элемент с классом "messages"
   const newMessage = document.createElement('div');
   newMessage.textContent = message;
-  messageBox.appendChild(newMessage);
-
-   // Удаляем сообщение через 5 секунд
+  messageBox.appendChild(newMessage); 
+// Удаляем сообщение через 5 секунд
  setTimeout(() => {
   messageBox.removeChild(newMessage);
 }, 5000);
@@ -55,3 +54,115 @@ getName((name, peerId) => {
 
 // Пример использования selfId
 console.log(`My information (${playerName},${selfId})`); 
+
+
+
+
+const byId = document.getElementById.bind(document)
+const canvas = byId('canvas')
+const peerInfo = byId('peer-info')
+const noPeersCopy = peerInfo.innerText
+const cursors = {}
+
+let mouseX = 0
+let mouseY = 0
+let sendMove
+let sendClick
+
+init(49)
+document.documentElement.className = 'ready'
+addCursor(selfId, true)
+
+addEventListener('mousemove', ({clientX, clientY}) => {
+  mouseX = clientX / innerWidth
+  mouseY = clientY / innerHeight
+  moveCursor([mouseX, mouseY], selfId)
+  if (room) {
+    sendMove([mouseX, mouseY])
+  }
+})
+
+addEventListener('touchstart', e => {
+  const x = e.touches[0].clientX / innerWidth
+  const y = e.touches[0].clientY / innerHeight
+  const payload = [randomFruit(), x, y]
+
+  
+  moveCursor([x, y], selfId)
+
+  if (room) {
+    sendMove([x, y])
+    sendClick(payload)
+  }
+})
+
+function init(n) {
+  let getMove
+  let getClick
+
+  room = joinRoom(config, 'room' + n)
+  ;[sendMove, getMove] = room.makeAction('mouseMove')
+  ;[sendClick, getClick] = room.makeAction('click')
+
+  byId('room-num').innerText = 'room #' + n
+  room.onPeerJoin(addCursor)
+  room.onPeerLeave(removeCursor)
+  getMove(moveCursor)
+  getClick(dropFruit)
+}
+
+function moveCursor([x, y], id) {
+  const el = cursors[id]
+
+  if (el && typeof x === 'number' && typeof y === 'number') {
+    el.style.left = x * innerWidth + 'px'
+    el.style.top = y * innerHeight + 'px'
+  }
+}
+
+function addCursor(id, isSelf) {
+  const el = document.createElement('div')
+  const img = document.createElement('img')
+  const txt = document.createElement('p')
+
+  el.className = `cursor${isSelf ? ' self' : ''}`
+  el.style.left = el.style.top = '-99px'
+  img.src = 'images/hand.png'
+  txt.innerText = isSelf ? 'you' : id.slice(0, 4)
+  el.appendChild(img)
+  el.appendChild(txt)
+  canvas.appendChild(el)
+  cursors[id] = el
+
+  if (!isSelf) {
+    sendMove([Math.random() * 0.93, Math.random() * 0.93], id)
+    updatePeerInfo()
+  }
+
+  return el
+}
+
+function removeCursor(id) {
+  if (cursors[id]) {
+    canvas.removeChild(cursors[id])
+  }
+  updatePeerInfo()
+}
+
+function updatePeerInfo() {
+  const count = Object.keys(room.getPeers()).length
+  peerInfo.innerHTML = count
+    ? `Right now <em>${count}</em> other peer${
+        count === 1 ? ' is' : 's are'
+      } connected with you. Click to send them some fruit.`
+    : noPeersCopy
+}
+
+  const el = document.createElement('div')
+  el.className = 'fruit'
+  el.innerText = fruit
+  el.style.left = x * innerWidth + 'px'
+  el.style.top = y * innerHeight + 'px'
+  canvas.appendChild(el)
+  setTimeout(() => canvas.removeChild(el), 3000)
+
